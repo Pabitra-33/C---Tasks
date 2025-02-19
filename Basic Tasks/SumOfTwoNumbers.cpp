5979902c649e6435a52848752793e5e3139cec46
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-20 00:08:25 +0530

    printed sum of two numbers by taking user input

---
+++ Basic Tasks/SumOfTwoNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Including the input-output stream header file
+// Using the standard namespace
+using namespace std; 
+// Start of the main function
+int main() 
+{
+    int first, second;
+    cout << "Enter first number: ";
+    cin >> first;
+    cout << "Enter second number: ";
+    cin >> second;
+    cout << "\n\n Printing the sum of two numbers :\n"; // Outputting a message for displaying the sum of two numbers
+	cout << "-----------------------------------\n"; // Outputting a separator line
+    cout << " The sum of "<< first <<" and "<< second << " is : "<< first+second <<"\n\n" ; // Calculating and outputting the sum of 29 and 30
+
+}

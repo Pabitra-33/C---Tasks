850edd7f640e9fa88cbff2a522bb5c96b3328221
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-19 22:58:45 +0530

    initial commit

---
+++ Basic Tasks/SeparateLine.cpp
@@ -0,0 +1,12 @@
+#include<iostream> //Including the input-output stream header file in our program
+
+using namespace std; // Using the standard namespace
+
+// Start of the main function
+int main() {
+    cout << "\n\n Printing a welcome text in a separate line :\n"; // Outputting a welcome message with newlines
+	cout << "----------------------------------------------\n"; // Outputting a separator line
+    cout << " Welcome to \n" ; // Outputting a welcome message in a new line
+    cout << " Google.com "<<endl ; // Outputting a website address and ending the line
+
+} // End of the main function

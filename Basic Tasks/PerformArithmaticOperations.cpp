df783628035aea96f95d7230b25dd0abd1b9bc58
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-27 17:24:28 +0530

    Performing arithmatic operations using two variables by taking user input

---
+++ Basic Tasks/PerformArithmaticOperations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << "Addition Operation: "<< a+b << endl;
+  cout << "Substraction Operation: "<< a-b << endl;
+  cout << "Multiplication Operation: "<< a*b << endl;
+  cout << "Dvision Operation: "<< a/b << endl;
+  
+  return 0;
+}

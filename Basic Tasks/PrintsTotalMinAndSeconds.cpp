68d468041112ce70855201ade5430095d0f8a518
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-27 17:31:12 +0530

    calculating total minutes and seconds from a given hour

---
+++ Basic Tasks/PrintsTotalMinAndSeconds.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int hour = 5;//initializing hour as 5
+    int minutes = hour * 60;
+    int seconds = minutes * 60;
+    cout << minutes << endl << seconds;//prints the values
+    return 0;
+}

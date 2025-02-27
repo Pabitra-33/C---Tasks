dd78db64772d4b7dd336e0346f5af2ee90eb605a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-27 17:32:32 +0530

    modified and some new comments for better readability

--- Basic Tasks/PrintsTotalMinAndSeconds.cpp
+++ Basic Tasks/PrintsTotalMinAndSeconds.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
-    int hour = 5;//initializing hour as 5
-    int minutes = hour * 60;
-    int seconds = minutes * 60;
+    int hour;
+    cin >> hour;//taking user input
+    int minutes = hour * 60;//calculating total minutes
+    int seconds = minutes * 60;//calculating total seconds
     cout << minutes << endl << seconds;//prints the values
+
     return 0;
 }

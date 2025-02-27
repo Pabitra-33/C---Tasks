6af106e2731cbe46059093b1c90c6b67cbd00447
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-27 17:28:43 +0530

    Conversion of temperature from celcius to kelvin using a formula by adding 273 into it

---
+++ Basic Tasks/TemperatureConverter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double temperature = 25.5;
+    cout << "Celsius - " << temperature << endl;
+    cout << "Kelvin - " << temperature + 273; //performing temperature conversion using formula by adding 273
+
+    return 0;
+}
